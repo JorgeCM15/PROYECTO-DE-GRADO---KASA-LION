67c081b42c1e8f5654480351571155914d0e3a93
JorgeCM15 <jcamargo89@uan.edu.co>
2026-04-24 15:23:03 -0500

    ruta server.js
    se corrige ruta server.js

--- backend/server.js
+++ backend/server.js
@@ -13,9 +13,10 @@ const pool = mysql.createPool({
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
   port: process.env.MYSQLPORT,
-  ssl: {
-    rejectUnauthorized: false
-  },
+  ssl: process.env.NODE_ENV === 'production'
+    ? { rejectUnauthorized: false }
+    : false,
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
